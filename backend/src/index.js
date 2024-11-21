8bbc17d080be9e246746a9bd68638a5cd260480d
arushree16 <142385693+arushree16@users.noreply.github.com>
2024-11-22 04:42:43 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import voiceNotesRoutes from "./routes/voiceNotesRoutes.js"; // Import the voice note routes
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
@@ -28,12 +28,6 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Use the voice notes route
-app.use("/api/voiceNotes", voiceNotesRoutes); // The upload endpoint is now accessible via `/api/voiceNotes/uploadVoiceNote`
-
-// Serve static files for uploads
-app.use("/uploads", express.static("uploads"));
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -42,8 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// Start the server
 server.listen(PORT, () => {
-  console.log("Server is running on PORT: " + PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
